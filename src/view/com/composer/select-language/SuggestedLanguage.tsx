122a7c11fbdcc5cddec0c101d910a7cb3072d00f
Sedat Kapanoglu <sedat@kapanoglu.com>
2025-01-14 12:33:06 -0800

    improve language detection accuracy

--- src/view/com/composer/select-language/SuggestedLanguage.tsx
+++ src/view/com/composer/select-language/SuggestedLanguage.tsx
@@ -56,7 +56,14 @@ export function SuggestedLanguage({
   }, [text, hasInteracted])
 
   useEffect(() => {
-    const textTrimmed = text.trim()
+    let textTrimmed = text.trim()
+
+    // remove the last word before guessing to prevent a half-written word 
+    // from botching the confidence of language detection.
+    const lastSpace = textTrimmed.lastIndexOf(' ')
+    if (lastSpace > 0) {
+      textTrimmed = textTrimmed.slice(0, lastSpace)
+    }
 
     // Don't run the language model on small posts, the results are likely
     // to be inaccurate anyway.
